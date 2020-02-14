2047df875848e1adf4c0bc15f53b9d0d5e646554
EngimaticGiant <tidus.tavis@gmail.com>
2020-02-13 23:39:07 -0600

    minor changes in text and options

--- app/src/main/java/com/example/questv3/MainActivity.java
+++ app/src/main/java/com/example/questv3/MainActivity.java
@@ -128,14 +128,14 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_log) {
             intent = new Intent(this,QuestLog.class);
             startActivity(intent);
-        } else if (id == R.id.nav_tools) {
+        } else if (id == R.id.nav_groups) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_friends) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_request) {
 
         }
 
